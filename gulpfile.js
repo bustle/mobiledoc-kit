5e39bb2dc1f57ed0c7280d1c77d1fe7bf820357a
gdub22 <garth22@gmail.com>
2014-07-22 16:50:20 -0400

    overhaul gulp build system. Include compiler in build

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,18 @@ var jshint = require('gulp-jshint');
 var qunit  = require('gulp-qunit');
 var less   = require('gulp-less');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
 var header = require('gulp-header');
-var footer = require('gulp-footer');
 var util   = require('gulp-util');
 var open   = require('gulp-open');
-var path   = require('path');
+var rimraf = require('gulp-rimraf');
+var insert = require('gulp-insert');
+var runSequence = require('run-sequence');
+
+// ------------------------------------------- 
 
 var pkg = require('./package.json');
+
+var allSrc = './src/**/*';
 
 var jsSrc = [
   './src/js/index.js',
@@ -28,6 +31,8 @@ var jsSrc = [
   './src/js/embed-intent.js'
 ];
 
+var jsExtSrc = './src/js/ext/*';
+
 var cssSrc = [
   './src/css/editor.less',
   './src/css/toolbar.less',
@@ -41,7 +46,6 @@ var distDest = './dist/';
 var jsDistName = 'content-kit-editor.js';
 var jsDistPath = distDest + jsDistName;
 var cssDistName = 'content-kit-editor.css';
-var cssDistPath = distDest + cssDistName;
 
 var testRunner = './tests/index.html';
 var demo       = './demo/index.html';
@@ -65,50 +69,78 @@ var iifeFooter = ['',
                   '}(this, document));',
                   ''].join('\n'); 
 
-gulp.task('lint', function() {
-  gulp.src(jsSrc)
-      .pipe(jshint('.jshintrc'))
-      .pipe(jshint.reporter('default'));
-});
 
-gulp.task('lint-built', ['build'], function() {
-  return gulp.src(jsDistPath)
+// JSHint javascript code linting
+gulp.task('lint', function() {
+  return gulp.src(jsSrc)
              .pipe(jshint('.jshintrc'))
              .pipe(jshint.reporter('default'));
 });
 
-gulp.task('build', function() {
-  gulp.src(jsSrc)
-      .pipe(concat(jsDistName))
-      .pipe(header(iifeHeader))
-      .pipe(header(banner, { pkg : pkg } ))
-      .pipe(footer(iifeFooter))
-      .pipe(gulp.dest(distDest));
-
-  gulp.src(cssSrc)
-      .pipe(concat(cssDistName))
-      .pipe(less())
-      .pipe(gulp.dest(distDest));
+// Concatenates and builds javascript source code
+gulp.task('build-js-src', function() {
+  return gulp.src(jsSrc)
+             .pipe(concat(jsDistName))
+             .pipe(insert.wrap(iifeHeader, iifeFooter))
+             .pipe(header(banner, { pkg : pkg } ))
+            . pipe(gulp.dest(distDest));
 });
 
-gulp.task('test', ['build', 'lint-built'], function() {
-  return gulp.src(testRunner)
-             .pipe(qunit());
+// Concatenates external js dependencies with built js source
+gulp.task('build-js-ext', ['build-js-src'], function() {
+  return gulp.src([jsDistPath, jsExtSrc])
+             .pipe(concat(jsDistName))
+             .pipe(gulp.dest(distDest));
 });
 
-gulp.task('test-browser', ['build', 'lint-built'], function(){
-  return gulp.src(testRunner)
-             .pipe(open('<% file.path %>')); 
+// Builds final js output in sequence (tasks are order dependent)
+gulp.task('build-js', ['build-js-src', 'build-js-ext'], function() {
+  return runSequence('build-js-src', 'build-js-ext');
 });
 
+// Compiles LESS and concatenates css
+gulp.task('build-css', function() {
+  return gulp.src(cssSrc)
+             .pipe(concat(cssDistName))
+             .pipe(less())
+             .pipe(gulp.dest(distDest));
+});
+
+// Builds the entire suite of js/css
+gulp.task('build', ['build-css', 'build-js']);
+
+// Runs QUnit tests
+gulp.task('test', ['build'], function() {
+  return gulp.src(testRunner).pipe(qunit());
+});
+
+// Opens the test runner in your default browser
+gulp.task('test-browser', ['build'], function(){
+  return gulp.src(testRunner).pipe(open('<% file.path %>')); 
+});
+
+// Opens the demo in your default browser
 gulp.task('demo', function(){
-  return gulp.src(demo)
-             .pipe(open('<% file.path %>')); 
+  return gulp.src(demo).pipe(open('<% file.path %>')); 
 });
 
-gulp.task('watch', function() {
-  gulp.watch(jsSrc.concat(cssSrc), ['build']);
+// Removes built output files
+gulp.task('clean', function() {
+  return gulp.src(distDest + '*', { read: false }).pipe(rimraf());
 });
 
+// Watches when js files change and automatically lints/builds
+gulp.task('watch-js', function() {
+  gulp.watch(jsSrc.concat(jsExtSrc), ['lint', 'build-js']);
+});
 
-gulp.task('default', ['lint', 'build', 'lint-built', 'test']);
+// Watches when css files change and automatically builds
+gulp.task('watch-css', function() {
+  gulp.watch(cssSrc, ['build-css']);
+});
+
+// Watches when any files change and automatically builds
+gulp.task('watch', ['watch-js', 'watch-css']);
+
+// Default task
+gulp.task('default', ['lint', 'build', 'test']);
