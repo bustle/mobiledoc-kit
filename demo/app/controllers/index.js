051d267773e797f63f0a06c6290ce072506b6a75
Cory Forsyth <cory.forsyth@gmail.com>
2015-10-23 11:27:20 -0400

    Fix demo updating on selection change

--- demo/app/controllers/index.js
+++ demo/app/controllers/index.js
@@ -2,26 +2,26 @@ import Ember from 'ember';
 
 import * as mobiledocs from '../mobiledocs/index';
 
-let { $, computed } = Ember;
+let { $ } = Ember;
 
 export default Ember.Controller.extend({
-  mobiledocName: 'simple',
-  mobiledoc: computed('mobiledocName', function() {
-    return mobiledocs[this.get('mobiledocName')];
-  }),
-  // This initial value for editedMobiledoc will be
-  // stomped by actual changing values upon didEdit.
-  editedMobiledoc: computed('mobiledoc', function() {
-    return this.get('mobiledoc');
-  }),
+  init() {
+    this._super.apply(this, arguments);
+    let mobiledoc = mobiledocs['simple'];
+    this.set('mobiledoc', mobiledoc);
+    this.set('editedMobiledoc', mobiledoc);
+  },
+
   actions: {
     changeMobiledoc() {
       let selectElement = $('#select-mobiledoc');
       let name = selectElement.val();
-      this.set('mobiledocName', name);
-      let mobiledoc = this.get('mobiledoc');
+      let mobiledoc = mobiledocs[name];
+
+      this.set('mobiledoc', mobiledoc);
       this.set('editedMobiledoc', mobiledoc);
     },
+
     didEdit(value) {
       this.set('editedMobiledoc', value);
     }
