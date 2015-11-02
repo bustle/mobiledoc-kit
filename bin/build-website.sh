7fbd43868773a67a9f93a596eadf5f212c25cec6
Cory Forsyth <cory.forsyth@gmail.com>
2015-11-02 14:51:42 -0500

    update website build script, demo prod config

--- bin/build-website.sh
+++ bin/build-website.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo "Building website..."
 cd demo/
+echo "Building website in $(pwd)"
 rm -rf node_modules/ bower_components/*
 npm install
 bower install
 ember build --environment production
-
-echo "Arranging built website in website/"
-rm -rf website
-mkdir website
-
-# rename demo/dist/index.html to be the base index.html
 cd ..
-cp -R demo/dist/* website/
+
+echo "Arranging built website in $(pwd)/website"
+rm -rf website
+mkdir -p website/demo
+
+echo "Copying $(pwd)/demo/dist/* to $(pwd)/website/demo"
+cp -R demo/dist/* website/demo/
 
 CURRENT_SHA="$(git rev-parse HEAD)"
 git add website/
 git commit -m "built website from $CURRENT_SHA"
-echo "Done."
+echo "Deployed to http://bustlelabs.github.io/content-kit-editor/demo/"
