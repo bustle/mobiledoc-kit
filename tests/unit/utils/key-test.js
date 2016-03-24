81e1cb0f37c2bcdba41c2deff9beccbe736f919a
Cory Forsyth <cory.forsyth@gmail.com>
2016-03-24 16:29:36 -0400

    [BUGFIX] up/down arrows in Firefox should not update mobiledoc

--- tests/unit/utils/key-test.js
+++ tests/unit/utils/key-test.js
@@ -1,6 +1,7 @@
 import Helpers from '../../test-helpers';
 import Key from 'mobiledoc-kit/utils/key';
 import { MODIFIERS } from 'mobiledoc-kit/utils/key';
+import Keycodes from 'mobiledoc-kit/utils/keycodes';
 
 const {module, test} = Helpers;
 
@@ -36,4 +37,16 @@ test('#hasModifier with SHIFT', (assert) => {
   assert.ok(!key.hasModifier(MODIFIERS.META), "META not pressed");
   assert.ok(!key.hasModifier(MODIFIERS.CTRL), "CTRL not pressed");
   assert.ok(key.hasModifier(MODIFIERS.SHIFT), "SHIFT pressed");
+});
+
+// Firefox will fire keypress events for up/down arrow keys,
+// they should not be considered printable
+test('firefox arrow keypress is not printable', (assert) => {
+  let element = $('#qunit-fixture')[0];
+  let event = Helpers.dom.createMockEvent('keypress', element, {
+    keyCode: Keycodes.DOWN,
+    charCode: 0
+  });
+  let key = Key.fromEvent(event);
+  assert.ok(!key.isPrintable());
 });
