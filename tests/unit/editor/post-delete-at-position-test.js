ddfed96880b376906106d5a31e4902441d26710e
Cory <cory.forsyth@gmail.com>
2016-08-25 11:06:41 -0400

    Refactor deletion tests to be terser

--- tests/unit/editor/post-delete-at-position-test.js
+++ tests/unit/editor/post-delete-at-position-test.js
@@ -7,11 +7,14 @@ const { module, test } = Helpers;
 
 let { postEditor: { run } } = Helpers;
 let { postAbstract: { buildFromText } } = Helpers;
+const { editor: { retargetPosition } } = Helpers;
 
 module('Unit: PostEditor: #deleteAtPosition');
 
-test('single markup section (backward)', (assert) => {
-  let examples = [
+let expectationGroups = [{
+  name: 'single markup section',
+  direction: BACKWARD,
+  expectations: [
     ['abc|def', 'ab|def', 'middle'],
     ['abcdef|', 'abcde|', 'end'],
     ['|abcdef', '|abcdef', 'start'],
@@ -21,23 +24,11 @@ test('single markup section (backward)', (assert) => {
 
     ['ab @| ef', 'ab | ef', 'atom (right side)'],
     ['ab |@ ef', 'ab|@ ef', 'atom (left side)']
-  ];
-
-  examples.forEach(([before, after, msg]) => {
-    let { post, range: { head: position } } = buildFromText(before);
-    let { post: expectedPost, range: { head: expectedPosition } } = buildFromText(after);
-
-    position = run(post, postEditor => postEditor.deleteAtPosition(position, BACKWARD));
-
-    expectedPosition = post.sections.head.toPosition(expectedPosition.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post ${msg}`);
-    assert.positionIsEqual(position, expectedPosition, `position ${msg}`);
-  });
-});
-
-test('single markup section (forward)', (assert) => {
-  let examples = [
+  ]
+}, {
+  name: 'single markup section',
+  direction: FORWARD,
+  expectations: [
     ['abc|def', 'abc|ef', 'middle'],
     ['abcdef|', 'abcdef|', 'end'],
     ['|abcdef', '|bcdef', 'start'],
@@ -47,129 +38,57 @@ test('single markup section (forward)', (assert) => {
 
     ['ab @| ef', 'ab @|ef', 'atom (right side)'],
     ['ab |@ ef', 'ab | ef', 'atom (left side)']
-  ];
-
-  examples.forEach(([before, after, msg]) => {
-    let { post, range: { head: position } } = buildFromText(before);
-    let { post: expectedPost, range: { head: expectedPosition } } = buildFromText(after);
-
-    position = run(post, postEditor => postEditor.deleteAtPosition(position, FORWARD));
-
-    expectedPosition = post.sections.head.toPosition(expectedPosition.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post ${msg}`);
-    assert.positionIsEqual(position, expectedPosition, `position ${msg}`);
-  });
-});
-
-test('across section boundary (backward)', (assert) => {
-  let examples = [
+  ]
+}, {
+  name: 'across section boundary',
+  direction: BACKWARD,
+  expectations: [
     [['abc','|def'], 'abc|def', 'markup sections'],
     [['*abc*','|def'], '*abc*|def', 'markup sections with markup'],
     [['[abc]','|def'], ['[abc]|','def'], 'prev section is card'],
     [['abc','|[def]'], ['abc|','[def]'], 'cur section is card'],
     [['', '|abc'], ['|abc'], 'prev section is blank']
-  ];
-
-  examples.forEach(([before, after, msg]) => {
-    let { post, range: { head: position } } = buildFromText(before);
-    let { post: expectedPost, range: { head: expectedPosition } } = buildFromText(after);
-
-    position = run(post, postEditor => postEditor.deleteAtPosition(position, BACKWARD));
-
-    expectedPosition = post.sections.head.toPosition(expectedPosition.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post ${msg}`);
-    assert.positionIsEqual(position, expectedPosition, `position ${msg}`);
-  });
-});
-
-test('across section boundary (forward)', (assert) => {
-  let examples = [
+  ]
+}, {
+  name: 'across section boundary',
+  direction: FORWARD,
+  expectations: [
     [['abc|','def'], 'abc|def', 'markup sections'],
     [['*abc*|','def'], '*abc*|def', 'markup sections with markup'],
     [['[abc]|','def'], ['[abc]|','def'], 'cur section is card'],
     [['abc|','[def]'], ['abc|','[def]'], 'next section is card'],
     [['abc|', ''], ['abc|'], 'next section is blank']
-  ];
-
-  examples.forEach(([before, after, msg]) => {
-    let { post, range: { head: position } } = buildFromText(before);
-    let { post: expectedPost, range: { head: expectedPosition } } = buildFromText(after);
-
-    position = run(post, postEditor => postEditor.deleteAtPosition(position, FORWARD));
-
-    expectedPosition = post.sections.head.toPosition(expectedPosition.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post ${msg}`);
-    assert.positionIsEqual(position, expectedPosition, `position ${msg}`);
-  });
-});
-
-test('across list item boundary (backward)', (assert) => {
-  let examples = [
+  ]
+}, {
+  name: 'across list item boundary',
+  direction: BACKWARD,
+  expectations: [
     [['* abc','* |def'], ['* abc', '|def'], 'start of list item'],
     [['* abc','|def'], ['* abc|def'], 'into list item'],
     [['', '* |abc'], ['', '|abc'], 'prev blank section'],
-  ];
-
-  examples.forEach(([before, after, msg]) => {
-    let { post, range: { head: position } } = buildFromText(before);
-    let { post: expectedPost, range: { head: expectedPosition } } = buildFromText(after);
-
-    position = run(post, postEditor => postEditor.deleteAtPosition(position, BACKWARD));
-
-    // FIXME need to figure out how to say which section to expect the range to include
-    let sectionIndex;
-    let index = 0;
-    expectedPost.walkAllLeafSections(section => {
-      if (section === expectedPosition.section) { sectionIndex = index; }
-      index++;
-    });
-
-    let section;
-    index = 0;
-    post.walkAllLeafSections(_section => {
-      if (index === sectionIndex) { section = _section; }
-      index++;
-    });
-    expectedPosition = section.toPosition(expectedPosition.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post ${msg}`);
-    assert.positionIsEqual(position, expectedPosition, `position ${msg}`);
-  });
-});
-
-test('across list item boundary (forward)', (assert) => {
-  let examples = [
+  ]
+}, {
+  name: 'across list item boundary',
+  direction: FORWARD,
+  expectations: [
     [['* abc|','* def'], ['* abc|def'], 'item into item'],
     [['* abc|','def'], ['* abc|def'], 'item into markup'],
     [['abc|','* def'], ['abc|def'], 'markup into markup'],
-  ];
+  ]
+}];
 
-  examples.forEach(([before, after, msg]) => {
-    let { post, range: { head: position } } = buildFromText(before);
-    let { post: expectedPost, range: { head: expectedPosition } } = buildFromText(after);
+expectationGroups.forEach(({name, expectations, direction}) => {
+  expectations.forEach(([before, after, msg]) => {
+    let dir = direction === FORWARD ? 'forward' : 'backward';
+    test(`${dir}: ${name}, "${before}" -> "${after}" (${msg})`, (assert) => {
+      let { post, range: { head: position } } = buildFromText(before);
+      let { post: expectedPost, range: { head: expectedPosition } } = buildFromText(after);
 
-    position = run(post, postEditor => postEditor.deleteAtPosition(position, FORWARD));
+      position = run(post, postEditor => postEditor.deleteAtPosition(position, direction));
+      expectedPosition = retargetPosition(expectedPosition, post);
 
-    // FIXME need to figure out how to say which section to expect the range to include
-    let sectionIndex;
-    let index = 0;
-    expectedPost.walkAllLeafSections(section => {
-      if (section === expectedPosition.section) { sectionIndex = index; }
-      index++;
+      assert.postIsSimilar(post, expectedPost);
+      assert.positionIsEqual(position, expectedPosition);
     });
-
-    let section;
-    index = 0;
-    post.walkAllLeafSections(_section => {
-      if (index === sectionIndex) { section = _section; }
-      index++;
-    });
-    expectedPosition = section.toPosition(expectedPosition.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post ${msg}`);
-    assert.positionIsEqual(position, expectedPosition, `position ${msg}`);
   });
 });
--- tests/unit/editor/post-delete-range-test.js
+++ tests/unit/editor/post-delete-range-test.js
@@ -1,96 +1,44 @@
 import Helpers from '../../test-helpers';
-import Range from 'mobiledoc-kit/utils/cursor/range';
-import { forEach } from 'mobiledoc-kit/utils/array-utils';
 
 const { module, test } = Helpers;
 
-let { postEditor: { run } } = Helpers;
-let { postAbstract: { buildFromText } } = Helpers;
+const { postEditor: { run } } = Helpers;
+const { postAbstract: { buildFromText } } = Helpers;
+const { editor: { retargetRange } } = Helpers;
 
 module('Unit: PostEditor: #deleteRange');
 
-test('#deleteRange with collapsed range is no-op', (assert) => {
+test('with collapsed range is no-op', (assert) => {
   let { post, range } = buildFromText('abc|def');
   let { post: expectedPost, range: expectedRange } = buildFromText('abc|def');
 
   let position = run(post, postEditor => postEditor.deleteRange(range));
-  let renderedRange = new Range(position);
 
-  expectedRange = Range.create(post.sections.head, expectedRange.head.offset);
+  expectedRange = retargetRange(expectedRange, post);
 
   assert.postIsSimilar(post, expectedPost);
-  assert.rangeIsEqual(renderedRange, expectedRange);
+  assert.rangeIsEqual(position.toRange(), expectedRange);
 });
 
-test('#deleteRange within a section (single section)', (assert) => {
-  let examples = [
+let expectationGroups = [{
+  name: 'within a section (single section)',
+  expectations: [
     ['ab<c>', 'ab|', 'at tail'],
     ['<a>bc', '|bc', 'at head'],
     ['a<b>c', 'a|c', 'middle']
-  ];
-
-  examples.forEach(([before, after, msg]) => {
-    let { post, range } = buildFromText(before);
-    let { post: expectedPost, range: expectedRange } = buildFromText(after);
-
-    let position = run(post, postEditor => postEditor.deleteRange(range));
-    let renderedRange = new Range(position);
-
-    expectedRange = Range.create(post.sections.head, expectedRange.head.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post (${msg})`);
-    assert.rangeIsEqual(renderedRange, expectedRange, `range (${msg})`);
-  });
-});
-
-test('#deleteRange within a section with markup (single section)', (assert) => {
-  let examples = [
+  ]
+}, {
+  name: 'within a section with markup (single section)',
+  expectations: [
     ['abc <*def*> ghi', 'abc | ghi', 'entire markup in middle'],
     ['abc *de<f* ghi>', 'abc *de|*', 'partial markup at end'],
     ['abc *de<f* g>hi', 'abc *de|*hi', 'partial markup in middle (right)'],
     ['ab<c *de>f* ghi', 'ab*|f* ghi', 'partial markup in middle (left)'],
     ['<abc *de>f* ghi', '*|f* ghi', 'partial markup at start'],
-  ];
-
-  examples.forEach(([before, after, msg]) => {
-    let { post, range } = buildFromText(before);
-    let { post: expectedPost, range: expectedRange } = buildFromText(after);
-
-    let position = run(post, postEditor => postEditor.deleteRange(range));
-    let renderedRange = new Range(position);
-
-    expectedRange = Range.create(post.sections.head, expectedRange.head.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post (${msg})`);
-    assert.rangeIsEqual(renderedRange, expectedRange, `range (${msg})`);
-  });
-});
-
-test('#deleteRange entire post', (assert) => {
-  let examples = [
-    [['<abc>'], 'single section'],
-    [['<[some-card]>'], 'single card'],
-    [['<abc','def','ghi>'], 'multiple sections'],
-    [['<>'], 'single blank section'],
-    [['<','','>'], 'multiple blank sections'],
-    [['<[some-card]', 'abc', '[some-card]>'], 'cards at head/tail, containing markup section'],
-    [['<abc', '[some-card]', 'def>'], 'markup sections containing card'],
-    [['<[some-card]', 'abc>'], 'card->markup'],
-    [['<abc', '[some-card]>'], 'markup->card'],
-  ];
-
-  examples.forEach(([text, msg]) => {
-    let { post, range } = buildFromText(text);
-    let position = run(post, postEditor => postEditor.deleteRange(range));
-
-    assert.ok(post.sections.length === 1 && post.sections.head.isBlank, `post has single blank section after deleteRange (${msg})`);
-    assert.ok(position.section === post.sections.head, `position#section is correct (${msg})`);
-    assert.equal(position.offset, 0, `position#offset is correct (${msg})`);
-  });
-});
-
-test('#deleteRange across markup section boundaries', (assert) => {
-  let examples = [
+  ]
+}, {
+  name: 'across markup section boundaries',
+  expectations: [
     [['abc<', '>def'], 'abc|def', 'at boundary'],
     [['abc<', 'd>ef'], 'abc|ef', 'boundary into next section'],
     [['ab<c', '>def'], 'ab|def', 'section into boundary'],
@@ -100,24 +48,10 @@ test('#deleteRange across markup section boundaries', (assert) => {
     [['abc<', 'def', 'g>hi'], 'abc|hi', 'across section boundary containing next section'],
     [['ab<c', 'def', '>ghi'], 'ab|ghi', 'across section boundary containing before section'],
     [['ab<c', 'def', 'g>hi'], 'ab|hi', 'across section boundary containing before and after section'],
-  ];
-
-  examples.forEach(([before, after, msg]) => {
-    let { post, range } = buildFromText(before);
-    let { post: expectedPost, range: expectedRange } = buildFromText(after);
-
-    let position = run(post, postEditor => postEditor.deleteRange(range));
-    let renderedRange = new Range(position);
-
-    expectedRange = Range.create(post.sections.head, expectedRange.head.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post ${msg}`);
-    assert.rangeIsEqual(renderedRange, expectedRange, `range ${msg}`);
-  }); 
-});
-
-test('#deleteRange across markup section boundaries including markups', (assert) => {
-  let examples = [
+  ]
+}, {
+  name: 'across markup section boundaries including markups',
+  expectations: [
     [['*abc*<', '>def'], '*abc*|def', 'at boundary (left markup)'],
     [['*abc*<', 'd>ef'], '*abc*|ef', 'boundary into next section (left markup)'],
     [['*ab<c*', '>def'], '*ab*|def', 'section into boundary (left markup)'],
@@ -137,24 +71,10 @@ test('#deleteRange across markup section boundaries including markups', (assert)
     [['abc<', '*def*', '*g*>hi'], 'abc|hi', 'across section boundary containing next section (into markup)'],
     [['ab<c', '*def*', '>*g*hi'], 'ab|*g*hi', 'across section boundary containing before section (into markup)'],
     [['ab<c', '*def*', '*g*>hi'], 'ab|hi', 'across section boundary containing before and after section (into markup)'],
-  ];
-
-  examples.forEach(([before, after, msg]) => {
-    let { post, range } = buildFromText(before);
-    let { post: expectedPost, range: expectedRange } = buildFromText(after);
-
-    let position = run(post, postEditor => postEditor.deleteRange(range));
-    let renderedRange = new Range(position);
-
-    expectedRange = Range.create(post.sections.head, expectedRange.head.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post ${msg}`);
-    assert.rangeIsEqual(renderedRange, expectedRange, `range ${msg}`);
-  }); 
-});
-
-test('#deleteRange across markup/non-markup section boundaries', (assert) => {
-  let examples = [
+  ]
+}, {
+  name: 'across markup/non-markup section boundaries',
+  expectations: [
     [['[some-card]<','>abc'], ['[some-card]|', 'abc'], 'card->markup start'], 
     [['[some-card]<','>'], ['[some-card]|'], 'card->blank-markup'], 
     [['<[some-card]','a>bc'], ['|bc'], 'card->markup inner'], 
@@ -166,31 +86,10 @@ test('#deleteRange across markup/non-markup section boundaries', (assert) => {
     [['abc', '<[some-card]>', 'def'], ['abc', '|', 'def'], 'containing card, boundaries in card section'],
 
     [['<', '>[some-card]'], ['|[some-card]'], 'blank section into card']
-  ];
-
-  examples.forEach(([before, after, msg]) => {
-    let { post, range } = buildFromText(before);
-
-    let { post: expectedPost, range: expectedRange } = buildFromText(after);
-
-    let position = run(post, postEditor => postEditor.deleteRange(range));
-    let renderedRange = new Range(position);
-
-    // FIXME need to figure out how to say which section to expect the range to include
-    let sectionIndex;
-    forEach(expectedPost.sections, (section, index) => {
-      if (section === expectedRange.head.section) { sectionIndex = index; }
-    });
-    expectedRange = Range.create(post.sections.objectAt(sectionIndex),
-                                 expectedRange.head.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post ${msg}`);
-    assert.rangeIsEqual(renderedRange, expectedRange, `range ${msg}`);
-  }); 
-});
-
-test('#deleteRange across list items', (assert) => {
-  let examples = [
+  ]
+}, {
+  name: 'across list items',
+  expectations: [
     [['* item 1<', '* >item 2'], ['* item 1|item 2'], 'at boundary'],
     [['* item <1', '* i>tem 2'], ['* item |tem 2'], 'surrounding boundary'],
     [['* item 1<', '* i>tem 2'], ['* item 1|tem 2'], 'boundary to next'],
@@ -214,56 +113,54 @@ test('#deleteRange across list items', (assert) => {
     [['* <','abc>'], ['* |'], 'empty li into markup end'],
     [['* abc<','>'], ['* abc|'], 'li into empty markup'],
     [['* <','>'], ['* |'], 'empty li into empty markup'],
-  ];
-
-  examples.forEach(([before, after, msg]) => {
-    let { post, range } = buildFromText(before);
-    let { post: expectedPost, range: expectedRange } = buildFromText(after);
-
-    let position = run(post, postEditor => postEditor.deleteRange(range));
-    let renderedRange = new Range(position);
-
-    // FIXME need to figure out how to say which section to expect the range to include
-    let sectionIndex;
-    let index = 0;
-    expectedPost.walkAllLeafSections(section => {
-      if (section === expectedRange.head.section) { sectionIndex = index; }
-      index++;
-    });
-
-    let section;
-    index = 0;
-    post.walkAllLeafSections(_section => {
-      if (index === sectionIndex) { section = _section; }
-      index++;
-    });
-    expectedRange = Range.create(section, expectedRange.head.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post ${msg}`);
-    assert.rangeIsEqual(renderedRange, expectedRange, `range ${msg}`);
-  });
-});
-
-test('#deleteRange with atoms', (assert) => {
-  let examples = [
+  ]
+}, {
+  name: 'with atoms',
+  expectations: [
     ['abc<@>def', 'abc|def', 'surrounding'],
     ['abc<@d>ef', 'abc|ef', 'into atom into next marker'],
     ['ab<c@>def', 'ab|def', 'into marker into atom'],
 
     ['ab<c>@def', 'ab|@def', 'prev boundary'],
     ['abc@<d>ef', 'abc@|ef', 'next boundary']
-  ];
+  ]
+}];
 
-  examples.forEach(([before, after, msg]) => {
-    let { post, range } = buildFromText(before);
-    let { post: expectedPost, range: expectedRange } = buildFromText(after);
+expectationGroups.forEach(({name, expectations}) => {
+  expectations.forEach(([before, after, msg]) => {
+    test(`${name}, "${before}" -> "${after}" (${msg})`, (assert) => {
+      let { post, range } = buildFromText(before);
+      let { post: expectedPost, range: expectedRange } = buildFromText(after);
 
+      let position = run(post, postEditor => postEditor.deleteRange(range));
+
+      expectedRange = retargetRange(expectedRange, post);
+
+      assert.postIsSimilar(post, expectedPost);
+      assert.rangeIsEqual(position.toRange(), expectedRange);
+    });
+  });
+});
+
+let entirePostExpectations = [
+  [['<abc>'], 'single section'],
+  [['<[some-card]>'], 'single card'],
+  [['<abc','def','ghi>'], 'multiple sections'],
+  [['<>'], 'single blank section'],
+  [['<','','>'], 'multiple blank sections'],
+  [['<[some-card]', 'abc', '[some-card]>'], 'cards at head/tail, containing markup section'],
+  [['<abc', '[some-card]', 'def>'], 'markup sections containing card'],
+  [['<[some-card]', 'abc>'], 'card->markup'],
+  [['<abc', '[some-card]>'], 'markup->card'],
+];
+
+entirePostExpectations.forEach(([text, msg]) => {
+  test(`entire post "${text}" (${msg})`, (assert) => {
+    let { post, range } = buildFromText(text);
     let position = run(post, postEditor => postEditor.deleteRange(range));
-    let renderedRange = new Range(position);
 
-    expectedRange = Range.create(post.sections.head, expectedRange.head.offset);
-
-    assert.postIsSimilar(post, expectedPost, `post ${msg}`);
-    assert.rangeIsEqual(renderedRange, expectedRange, `range ${msg}`);
+    assert.ok(post.sections.length === 1 && post.sections.head.isBlank, `post has single blank section after deleteRange (${msg})`);
+    assert.ok(position.section === post.sections.head, `position#section is correct (${msg})`);
+    assert.equal(position.offset, 0, `position#offset is correct (${msg})`);
   });
 });
