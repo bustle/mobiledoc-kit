059fd66d38dc4f199c5e5984eabf2f53348ef4a9
Cory Forsyth <cory.forsyth@gmail.com>
2016-09-06 17:28:29 -0400

    fix(deprecate): Make deprecate accept conditional argument (#488)

--- src/js/utils/deprecate.js
+++ src/js/utils/deprecate.js
@@ -1,3 +1,14 @@
-export default function deprecate(message) {
-  console.log(`[mobiledoc-kit] [DEPRECATED]: ${message}`); // jshint ignore:line
+/**
+ * Usage:
+ * Without a conditional, always prints deprecate message:
+ *   `deprecate('This is deprecated')`
+ *
+ * Conditional deprecation, works similarly to `assert`, prints deprecation if
+ * conditional is false:
+ *   `deprecate('Deprecated only if foo !== bar', foo === bar)`
+ */
+export default function deprecate(message, conditional=false) {
+  if (!conditional) {
+    console.log(`[mobiledoc-kit] [DEPRECATED]: ${message}`); // jshint ignore:line
+  }
 }
