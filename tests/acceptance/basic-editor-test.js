583d3f649c2a76d44e5a6f80467cd8f9e1f4119d
Dale Ma <dalema22@gmail.com>
2016-10-11 23:51:24 -0700

    remove range parameter

--- tests/acceptance/basic-editor-test.js
+++ tests/acceptance/basic-editor-test.js
@@ -214,7 +214,7 @@ test('keypress events when the editor does not have selection are ignored', (ass
 test('prevent handling newline', (assert) => {
   editor = Helpers.editor.buildFromText('', {element: editorElement});
 
-  editor.willHandleNewline((event, editor, range) => {
+  editor.willHandleNewline((event, {range}) => {
     assert.equal(range.head.section.text, 'Line1', 'can access section text');
     assert.ok(true, 'willHandleNewline should be triggered');
     event.preventDefault();
