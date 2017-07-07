533fa990250ec8236d405e529be7a69ab0183f24
Yoran Brondsema <yoran@sutori.com>
2017-07-07 12:07:20 -0500

    Remove rule newline-per-chained-call in tests

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   "rules": {
     "complexity": "off",
     "dot-location": "off",
-    "newline-per-chained-call": "off",
     "no-new": "off",
     "no-use-before-define": "off",
     "no-unused-vars": "off"
