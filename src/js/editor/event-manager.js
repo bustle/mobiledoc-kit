5388a88e47e7b0f581c90723791346c5f3f4e849
iLoveBug <fan.zhang@me.com>
2019-12-19 11:41:05 +0800

    Fix IME problem - especially for CJKT. Verified OK on Chrome. Known issue for Safari: the first chinese world is duplicated.

--- src/js/editor/event-manager.js
+++ src/js/editor/event-manager.js
@@ -9,9 +9,10 @@ import Key from 'mobiledoc-kit/utils/key';
 import TextInputHandler from 'mobiledoc-kit/editor/text-input-handler';
 import SelectionManager from 'mobiledoc-kit/editor/selection-manager';
 import Browser from 'mobiledoc-kit/utils/browser';
+import TextParser from '../parsers/text';
 
 const ELEMENT_EVENT_TYPES = [
-  'keydown', 'keyup', 'cut', 'copy', 'paste', 'keypress', 'drop'
+  'keydown', 'keyup', 'cut', 'copy', 'paste', 'keypress', 'drop', 'compositionstart', 'compositionend'
 ];
 
 export default class EventManager {
@@ -20,6 +21,7 @@ export default class EventManager {
     this.logger = editor.loggerFor('event-manager');
     this._textInputHandler = new TextInputHandler(editor);
     this._listeners = [];
+    this._isComposing = false;
     this.modifierKeys = {
       shift: false
     };
@@ -153,6 +155,7 @@ export default class EventManager {
     let { editor } = this;
     if (!editor.hasCursor()) { return; }
     if (!editor.isEditable) { return; }
+    if (this._isComposing) { return; }
 
     let key = Key.fromEvent(event);
     this._updateModifiersFromKey(key, {isDown:true});
@@ -166,6 +169,10 @@ export default class EventManager {
     let range = editor.range;
 
     switch(true) {
+      // Ignore IME
+      case event.isComposing || key.isIME(): {
+        break;
+      }
       // FIXME This should be restricted to only card/atom boundaries
       case key.isHorizontalArrowWithoutModifiersOtherThanShift(): {
         let newRange;
@@ -210,6 +217,43 @@ export default class EventManager {
     this._updateModifiersFromKey(key, {isDown:false});
   }
 
+  compositionstart() {
+    event.preventDefault();
+    this._isComposing = true;
+    let { editor } = this;
+
+    editor._mutationHandler.stopObserving();
+    this._selectionManager.stop();
+
+    if (editor.post.isBlank) {
+      editor._insertEmptyMarkupSectionAtCursor();
+    }
+
+    this._editorRangeOffset = editor.range.head.offset;
+  }
+
+  compositionend(event) {
+    event.preventDefault();
+
+    this._isComposing = false;
+
+    let { editor } = this;
+    let { builder, _parserPlugins: plugins } = editor;
+    let parser = new TextParser(builder, {plugins});
+    let post = parser.parse(event.data);
+
+    let position = editor.range.head;
+    position.offset = this._editorRangeOffset;
+
+    editor.run(postEditor => {
+      let nextPosition = postEditor.insertPost(position, post);
+      postEditor.setRange(nextPosition);
+    });
+
+    this.editor._mutationHandler.startObserving();
+    this._selectionManager.start();
+  }
+
   cut(event) {
     event.preventDefault();
 
@@ -249,7 +293,6 @@ export default class EventManager {
     let position = editor.range.head;
     let targetFormat = this.modifierKeys.shift ? 'text' : 'html';
     let pastedPost = parsePostFromPaste(event, editor, {targetFormat});
-
     editor.run(postEditor => {
       let nextPosition = postEditor.insertPost(position, pastedPost);
       postEditor.setRange(nextPosition);
