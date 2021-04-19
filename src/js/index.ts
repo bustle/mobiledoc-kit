d56a11335c01565cf08cf0a46451285e40e35401
Ruslan Zavacky <ruslan.zavacky@crowdstrike.com>
2021-04-19 13:37:05 +0100

    Add exports to allow to build additional use-cases on top of mobiledoc-kit

--- src/js/index.ts
+++ src/js/index.ts
@@ -5,6 +5,8 @@ import Range from './utils/cursor/range'
 import Position from './utils/cursor/position'
 import Error from './utils/mobiledoc-error'
 import VERSION from './version'
-import { MOBILEDOC_VERSION } from './renderers/mobiledoc'
+import mobiledocRenderers, { MOBILEDOC_VERSION } from './renderers/mobiledoc'
+import DOMParser from './parsers/dom'
+import PostNodeBuilder from './models/post-node-builder'
 
-export { Editor, UI, ImageCard, Range, Position, Error, VERSION, MOBILEDOC_VERSION }
+export { Editor, UI, ImageCard, Range, Position, Error, VERSION, MOBILEDOC_VERSION, DOMParser, PostNodeBuilder, mobiledocRenderers }
